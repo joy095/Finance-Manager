8ea810130813232d8a5b584b9b08ce7ea9c86458
Joy Karmakar <96533682+joy095@users.noreply.github.com>
2025-02-21 00:31:10 +0530

    update

--- server/src/config/cors.js
+++ server/src/config/cors.js
@@ -3,22 +3,30 @@
 const whitelist = [
   "http://localhost:5173",
   "http://localhost:4173",
-  "https://finance-manager-xxjq.vercel.app/",
+  "https://finance-manager-xxjq.vercel.app",
 ];
 
 const corsOptions = {
   origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      // for dev
-      // if (!origin || whitelist.indexOf(origin) !== -1) {
+    // For development and testing - allow requests with no origin (like mobile apps or curl requests)
+    if (!origin || whitelist.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
-      callback(new Error("Not allowed by CORS"));
+      callback(new Error(`Not allowed by CORS: ${origin}`));
     }
   },
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization", "x-refresh-token"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "x-refresh-token",
+    "Access-Control-Allow-Origin",
+    "Access-Control-Allow-Credentials",
+  ],
+  exposedHeaders: ["set-cookie"],
+  preflightContinue: true,
+  optionsSuccessStatus: 204,
 };
 
 module.exports = corsOptions;
