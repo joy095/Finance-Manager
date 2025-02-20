bdf299e48c0458e43ecd679325343f82e3fad1e5
Joy Karmakar <96533682+joy095@users.noreply.github.com>
2025-02-21 01:09:48 +0530

    update

--- server/src/config/cors.js
+++ server/src/config/cors.js
@@ -1,28 +1,11 @@
 /** @format */
 
-const whitelist = [
-  "http://localhost:5173",
-  "http://localhost:4173",
-  "https://finance-manager-xxjq.vercel.app",
-  "https://finance-manager-kzz6.vercel.app",
-];
+const allowedOrigins = "https://finance-manager-xxjq.vercel.app";
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    // Check if origin is in whitelist or if it's a development request (no origin)
-    if (!origin || whitelist.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error(`Not allowed by CORS: ${origin}`));
-    }
-  },
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization", "x-refresh-token"],
-  exposedHeaders: ["set-cookie", "Authorization"],
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-  maxAge: 86400,
-};
-
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true, // If using cookies/auth headers
+  })
+);
 module.exports = corsOptions;
