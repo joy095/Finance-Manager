e2b9849d4dac62ccc5a569c3c824b29f6f99b08f
Joy Karmakar <96533682+joy095@users.noreply.github.com>
2025-02-21 01:38:27 +0530

    update CORS configuration to allow requests from a specific origin and add headers in Vercel configuration

--- server/src/config/cors.js
+++ server/src/config/cors.js
@@ -1,26 +1,13 @@
 /** @format */
 
-const whitelist = [
-  "http://localhost:5173",
-  "http://localhost:4173",
-  "https://finance-manager-xxjq.vercel.app",
-];
-
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: "https://finance-manager-xxjq.vercel.app",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: [
     "Content-Type",
     "Authorization",
     "x-refresh-token",
-    "authorization",
   ],
 };
 
