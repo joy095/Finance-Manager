5745e5cfc91c7870d4e7044cc676f7a9179c6a2a
Joy Karmakar <96533682+joy095@users.noreply.github.com>
2025-02-21 01:28:08 +0530

    added

--- server/src/config/cors.js
+++ server/src/config/cors.js
@@ -4,12 +4,12 @@ const whitelist = [
   "http://localhost:5173",
   "http://localhost:4173",
   "https://finance-manager-xxjq.vercel.app",
+  "https://finance-manager-kzz6.vercel.app",
 ];
 
 const corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
-      // if (!origin || whitelist.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
