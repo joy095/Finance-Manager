ec5719fd8c76c2a621c7d91ed74caf3b2d289219
Joy Karmakar <96533682+joy095@users.noreply.github.com>
2025-02-21 00:20:47 +0530

    update

--- server/src/config/cors.js
+++ server/src/config/cors.js
@@ -8,8 +8,9 @@ const whitelist = [
 
 const corsOptions = {
   origin: function (origin, callback) {
-    // if (whitelist.indexOf(origin) !== -1) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
+    if (whitelist.indexOf(origin) !== -1) {
+      // for dev
+      // if (!origin || whitelist.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
